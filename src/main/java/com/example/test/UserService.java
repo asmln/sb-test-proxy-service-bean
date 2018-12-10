c851b6ec3edb0954e9a7021649c4a2d35cee0c3a
samoylenkoa <samoylenkoa@cmlt.ru>
2018-12-10 12:33:01 +0300

    fix comment

--- src/main/java/com/example/test/UserService.java
+++ src/main/java/com/example/test/UserService.java
@@ -4,8 +4,8 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 // @Service conflicts with @Bean (TestApplication.userService(...))
-// and Spring reports that "The bean 'userService', defined in com.example.test.TestApplication, could not be registered."
-// properties spring.main.allow-bean-definition-overriding=true fix this problem, but better to remove annotation @Service
+// and Spring reports that "The bean 'userService', defined in com.example.test.TestApplication, could not be registered..."
+// property spring.main.allow-bean-definition-overriding=true fix this problem, but better to remove annotation @Service from this class
 @Service
 public class UserService {
     String name;
